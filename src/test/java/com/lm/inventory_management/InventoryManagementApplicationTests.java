19b7f6751ca09cd4e7f7d76520f7058dc27ac557
Lucas <lucasdecassia9@hotmail.com>
2025-08-11 16:28:59 -0300

    Initial commit

---
+++ src/test/java/com/lm/inventory_management/InventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lm.inventory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
